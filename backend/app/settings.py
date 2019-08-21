a7d802a5cb81e798dded2ddcedccf4a6494c991d
mr.P <qkrcjfwls0923@gmail.com>
2019-08-21 17:48:13 +0900

    feat: Add corsheaders middleware

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'server',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'app.urls'
@@ -121,3 +123,9 @@ USE_TZ = False
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+CORS_ORIGIN_ALLOW_ALL=False
+CORS_ORIGIN_WHITELIST = [
+    'http://google.com',
+    'https://google.com',
+    'http://localhost:3000'
+]
